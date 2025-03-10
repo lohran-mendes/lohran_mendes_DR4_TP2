2241bc796d8b607d8660ff3716d2073fc041b402
Lohran Mendes <lohranfm@gmail.com>
2025-03-10 12:52:21 -0300

    chore: solved questions 11 and 12

--- script_11.js
+++ script_11.js
@@ -7,3 +7,35 @@
 // Valide sempre as entradas de dados.
 // Utilize prompt e alert.
 // Teste o seu código.
+
+const expressao = "(23 + 4) * (67 - 9)";
+
+function validaExpressao(entrada) {
+  let arrayDeValidacao = [];
+  let expressaoValida = true;
+  if (typeof entrada === "string") {
+    const arrayDaExpressao = [...entrada.replaceAll(" ", "").split("")];
+    arrayDaExpressao.forEach((valor) => {
+      switch (valor) {
+        case "(":
+          arrayDeValidacao.push(valor);
+          break;
+        case ")":
+          if (arrayDeValidacao.length > 0) {
+            arrayDeValidacao.pop();
+          } else {
+            expressaoValida = false;
+            return;
+          }
+          break;
+        default:
+          break;
+      }
+    });
+    if (arrayDeValidacao.length === 0 && expressaoValida) {
+      alert(`A expressão ${entrada} está correta!`);
+    } else alert(`A expressão ${entrada} está errada.`);
+  } else alert("É necessário que a entrada seja uma string!");
+}
+
+validaExpressao(expressao);
--- script_12.js
+++ script_12.js
@@ -13,3 +13,94 @@
 // Utilize console.log para exibir os números ao usuário.
 // Utilize prompt para perguntar ao usuário.
 // Todo jogo só tem 3 resultados possíveis: vitória, empate ou derrota. Pense nisso na hora de fazer a média.
+
+const nomeDoTime = prompt("Informe aqui o nome do time de futebol");
+const numeroDeVitorias = prompt("Informe aqui o número de vitórias");
+const numeroDeDerrotas = prompt("Informe aqui o número de derrotas");
+const numeroDeEmpates = prompt("Informe aqui o número de empates");
+
+function pontuacaoTime(nome, qtdVitorias, qtdDerrotas, qtdEmpates) {
+  let vitorias;
+  let derrotas;
+  let empates;
+
+  if (typeof nome !== "string" || nome.trim().length === 0) {
+    console.log(
+      "O nome do time não é válido, deve ser uma string que não seja vazia"
+    );
+    return;
+  }
+  if (
+    typeof qtdVitorias === "number" &&
+    Number.isInteger(qtdVitorias) &&
+    qtdVitorias >= 0
+  ) {
+    vitorias = qtdVitorias;
+  } else if (
+    qtdVitorias.trim().length > 0 &&
+    !isNaN(qtdVitorias) &&
+    Number.isInteger(Number(qtdVitorias)) &&
+    Number(qtdVitorias) >= 0
+  ) {
+    vitorias = Number(qtdVitorias);
+  } else {
+    console.log(
+      "A quantidade de vitórias não é válida, deve ser um número inteiro maior que zero!"
+    );
+    return;
+  }
+
+  if (
+    typeof qtdDerrotas === "number" &&
+    Number.isInteger(qtdDerrotas) &&
+    qtdDerrotas >= 0
+  ) {
+    derrotas = qtdDerrotas;
+  } else if (
+    qtdDerrotas.trim().length > 0 &&
+    !isNaN(qtdDerrotas) &&
+    Number.isInteger(Number(qtdDerrotas)) &&
+    Number(qtdDerrotas) >= 0
+  ) {
+    derrotas = Number(qtdDerrotas);
+  } else {
+    console.log(
+      "A quantidade de derrotas não é válida, deve ser um número inteiro maior que zero!"
+    );
+    return;
+  }
+  if (
+    typeof qtdEmpates === "number" &&
+    Number.isInteger(qtdEmpates) &&
+    qtdEmpates >= 0
+  ) {
+    empates = qtdEmpates;
+  } else if (
+    qtdEmpates.trim().length > 0 &&
+    !isNaN(qtdEmpates) &&
+    Number.isInteger(Number(qtdEmpates)) &&
+    Number(qtdEmpates) >= 0
+  ) {
+    empates = Number(qtdEmpates);
+  } else {
+    console.log(
+      "A quantidade de empates não é válida, deve ser um número inteiro maior que zero!"
+    );
+    return;
+  }
+
+  const pontosSobreVitoria = 3 * vitorias;
+  const qtdDePontosTotais = pontosSobreVitoria + empates;
+  const qtdDeJogos = vitorias + empates + derrotas;
+  const mediaDePontosPorJogo = (qtdDePontosTotais / qtdDeJogos).toFixed(2);
+
+  console.log(`${nome} disputou ${qtdDeJogos} jogos`);
+  console.log(`Fez um total de ${qtdDePontosTotais} pontos`);
+  console.log(
+    `E teve uma média de ${
+      mediaDePontosPorJogo > 0 ? mediaDePontosPorJogo : "nenhum"
+    } pontos por jogo`
+  );
+}
+
+pontuacaoTime(nomeDoTime, numeroDeVitorias, numeroDeDerrotas, numeroDeEmpates);
