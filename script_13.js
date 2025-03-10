e8e58524e685fb59b5af776ca4e0a96850bda9da
Lohran Mendes <lohranfm@gmail.com>
2025-03-10 14:04:48 -0300

    chore: solved questions 13 and 14

--- script_13.js
+++ script_13.js
@@ -11,3 +11,30 @@
 // Utilize console.log para exibir os números ao usuário.
 // Utilize prompt para perguntar ao usuário.
 // Os números sorteados devem ser aleatórios, entre 1 e 60.
+
+const qtdNumerosSorteados = Number(
+  prompt("Quantos números você deseja sortear?")
+);
+
+function sorteaNumeros(quantidade) {
+  const numerosSortados = [];
+  if (
+    !isNaN(qtdNumerosSorteados) &&
+    Number.isInteger(qtdNumerosSorteados) &&
+    qtdNumerosSorteados >= 1 &&
+    qtdNumerosSorteados <= 60
+  ) {
+    for (let i = 0; i < quantidade; i++) {
+      let addNumber = Math.ceil(Math.random() * 60);
+      if (!numerosSortados.includes(addNumber)) {
+        numerosSortados.push(addNumber);
+      } else i--;
+    }
+    console.log(numerosSortados);
+  } else
+    alert(
+      `${qtdNumerosSorteados} Não é um valor válido, lembre-se de entrar com um valor numérico inteiro entre 1 e 60!`
+    );
+}
+
+sorteaNumeros(qtdNumerosSorteados);
--- script_14.js
+++ script_14.js
@@ -4,3 +4,31 @@
 
 // Crie uma função de seta (arrow function), para calcular a soma entre três valores, informados pelo usuário através do prompt.
 // Chame esta função e exiba o resultado.
+
+const primeiroValor = prompt("Insira aqui o primeiro valor da soma");
+const segundoValor = prompt("Insira aqui o segundo valor da soma");
+const terceiroValor = prompt("Insira aqui o terceiro valor da soma");
+
+const funcaoDeSeta = (a, b, c) => {
+  if (typeof a === "string" && !isNaN(a) && a.trim().length > 0) {
+    a = Number(a);
+  } else {
+    alert("O primeiro valor não é válido, precisa ser um valor numérico!");
+    return;
+  }
+  if (typeof b === "string" && !isNaN(b) && b.trim().length > 0) {
+    b = Number(b);
+  } else {
+    alert("O segundo valor não é válido, precisa ser um valor numérico!");
+    return;
+  }
+  if (typeof c === "string" && !isNaN(c) && c.trim().length > 0) {
+    c = Number(c);
+  } else {
+    alert("O terceiro valor não é válido, precisa ser um valor numérico!");
+    return;
+  }
+  alert(`O resultado da soma de ${a}, ${b} e ${c} é igual a = ${a + b + c}`);
+};
+
+funcaoDeSeta(primeiroValor, segundoValor, terceiroValor);
